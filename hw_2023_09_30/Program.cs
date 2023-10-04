900d6b9a4a1d4767149fb7111ef74644fadf68c3
RFozzy688 <pokerfozzy@gmail.com>
2023-10-04 12:46:54 +0300

    start

---
+++ hw_2023_09_30/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_2023_09_30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
