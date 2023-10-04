910cc692824b99f3249ad6c390a30a330d53e100
RFozzy688 <pokerfozzy@gmail.com>
2023-10-04 20:00:57 +0300

    ListBooksAvailable(), UserListOfBooks(), ListOfBooksTakenRecently()

--- hw_2023_09_30/Program.cs
+++ hw_2023_09_30/Program.cs
@@ -23,8 +23,17 @@ namespace hw_2023_09_30
             Console.WriteLine("Список должников:");
             pr.ListOfDebtors();
 
-            Console.WriteLine("список авторов книги №3");
+            Console.WriteLine("Список авторов книги №3:");
             pr.ListOfBookAuthors();
+
+            Console.WriteLine("Список доступных книг:");
+            pr.ListBooksAvailable();
+
+            Console.WriteLine("Список книг, которые на руках у пользователя:");
+            pr.UserListOfBooks();
+
+            Console.WriteLine("Список книг, взятых за последнее время:");
+            pr.ListOfBooksTakenRecently();
         }
         public void ListOfDebtors() // список должников
         {
@@ -82,9 +91,110 @@ namespace hw_2023_09_30
                 {
                     Console.WriteLine($"{rdr[0]} {rdr[1]}");
                 }
+
+                Console.WriteLine();
             }
             finally
             {
+                rdr?.Close();
+                conn?.Close();
+            }
+        }
+        public void ListBooksAvailable() // список доступных книг
+        {
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                string str = "SELECT Book.name" +
+                    " FROM Book" +
+                    " WHERE Book.quantity > 0";
+
+                cmd.CommandText = str;
+                cmd.Connection = conn;
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine($"{rdr[0]}");
+                }
+
+                Console.WriteLine();
+            }
+            finally
+            {
+                rdr?.Close();
+                conn?.Close();
+            }
+        }
+        public void UserListOfBooks() // список книг, которые на руках у пользователя 
+        {
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                string strQuery = "SELECT Book.name" +
+                    " FROM Student JOIN S_Cards ON Student.id = S_Cards.id_student" +
+                    " JOIN Book ON S_Cards.id_book = Book.id" +
+                    " WHERE Student.id = @numStudent AND S_Cards.date_in IS NULL";
+
+                cmd.CommandText = strQuery;
+                cmd.Connection = conn;
+                cmd.Parameters.Add("@numStudent", System.Data.SqlDbType.Int).Value = 17;
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine($"{rdr[0]}");
+                }
+
+                Console.WriteLine();
+            }
+            finally
+            {
+                rdr?.Close();
+                conn?.Close();
+            }
+        }
+        public void ListOfBooksTakenRecently() // список книг, взятых за последнее время
+        {
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+
+                string strQuery = "SELECT DISTINCT Book.name" +
+                    " FROM Book JOIN S_Cards ON Book.id = S_Cards.id_book" +
+                    " WHERE S_Cards.date_out > @dateOut";
+
+                cmd.CommandText = strQuery;
+                cmd.Connection = conn;
+                cmd.Parameters.Add("@dateOut", System.Data.SqlDbType.Date).Value = "2023-09-25";
+
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine($"{rdr[0]}");
+                }
+
+                Console.WriteLine();
+            }
+            finally
+            {
+                rdr?.Close();
                 conn?.Close();
             }
         }
